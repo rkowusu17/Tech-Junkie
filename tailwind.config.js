f95d36522d4ff485f11635c330281f5056b830f9
Reginald Owusu <reginaldmiles45@gmail.com>
2024-09-15 23:33:50 +0000

    a littlre bit of changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "Float-in": {
+          "0%": { tramsform: "translateX(-200x) ", opacity: " 0%  " },
+          "30%": {
+            tramsform: "translateX(-150px)",
+            opacity: "40%",
+          },
+          "60%": {
+            tramsform: "translateX(-100px)",
+            opacity: "75%",
+          },
+          "90%": {
+            transform: "traslateX(-10px)",
+            opacity: "90%",
+          },
+          "100%": {
+            opacity: "100%",
+          },
+        },
+      },
+      animation: {
+        "Float-in": "Float-in 3s ",
+      },
+    },
   },
   plugins: [],
 };
